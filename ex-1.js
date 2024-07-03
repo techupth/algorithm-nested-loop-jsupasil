9308206d226b1b95b019b808de81c04f80e11f28
CharlieHii <charlie_chaper@hotmail.com>
2024-07-03 11:35:26 +0700

    feat: finish all assignment

--- ex-1.js
+++ ex-1.js
@@ -1,4 +1,14 @@
-// เริ่มเขียนโค้ดตรงนี้ 
-function printPattern() {
-   
+// เริ่มเขียนโค้ดตรงนี้
+function printPattern(n) {
+  let finalResult = "";
+  for (let i = 1; i <= n; i += 1) {
+    let rowResult = "";
+    for (let j = 1; j <= i; j += 1) {
+      rowResult += j + " ";
+    }
+    finalResult += rowResult + "\n";
+  }
+  return finalResult;
 }
+
+console.log(printPattern(5));
--- ex-3.js
+++ ex-3.js
@@ -1,4 +1,14 @@
-// เริ่มเขียนโค้ดตรงนี้ 
-function multiplicationTable() {
-   
+// เริ่มเขียนโค้ดตรงนี้
+function multiplicationTable(n) {
+  let finalResult = "";
+  for (let i = 1; i <= n; i += 1) {
+    let rowResult = [];
+    for (let j = 1; j <= n; j += 1) {
+      rowResult += i * j + "\t";
+    }
+    finalResult += rowResult + "\n";
+  }
+  return finalResult;
 }
+
+console.log(multiplicationTable(5));
